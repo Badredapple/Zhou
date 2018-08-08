05c37658c9fd840685b9fa6cae1aafd8735fa171
badredapple <zhouzhou@1197236910@qq.com>
2018-08-09 01:36:31 +0800

    googletest unit in clang++

---
+++ gcc_test/high-perform/C++/gtest.cc
@@ -0,0 +1,21 @@
+//gtest use 
+#include <gtest/gtest.h>
+
+int Gcd(int a, int b)
+{
+    return 0 == b ? a : Gcd(b,a%b);
+}
+
+TEST(GcdTest, IntTest)
+{
+    EXPECT_EQ(1, Gcd(2,5));
+    EXPECT_EQ(2, Gcd(2,5));
+    EXPECT_EQ(2, Gcd(2,4));
+    EXPECT_EQ(3, Gcd(6,9));
+}
+
+int main(int argc, char **argv){
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
